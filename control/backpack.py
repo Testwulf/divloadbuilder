ca909dd9060772d8e3d4109eef9245706ad8e282
Tester <testwulf@gmail.com>
2018-01-19 15:30:21 -0500

    Integrated new slot attribute for items into add item function. Added function to return current inventory. Added static collection of possible slot values.

--- control/backpack.py
+++ control/backpack.py
@@ -6,10 +6,21 @@ from model.backpack import Item
 
 # Master Inventory List
 INVENTORY = []
+# Possible Gear Slots
+SLOTS = [
+    'Body Armor',
+    'Knee Pads',
+    'Masks'
+]
 
 
-def add_item(name):
+def add_item(name, slot):
     '''Add a new item with the given name to the backpack'''
-    new_item = Item(name)
+    new_item = Item(name, slot)
     INVENTORY.append(new_item)
     return True
+
+
+def get_items():
+    '''Returns the current inventory'''
+    return INVENTORY
