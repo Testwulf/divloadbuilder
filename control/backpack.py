49703818f2d6720794f01f120515844809985dd4
Tester <testwulf@gmail.com>
2018-01-19 16:58:13 -0500

    Refactored SLOTS list to a dict defining the item type to be used for instantiation. Refactored add item function to add gear.

--- control/backpack.py
+++ control/backpack.py
@@ -1,23 +1,23 @@
 '''The backpack and inventory controller'''
 
 
-from model.backpack import Item
+from model.backpack import Item, Gear
 
 
 # Master Inventory List
 INVENTORY = []
 # Possible Gear Slots
-SLOTS = [
-    'Body Armor',
-    'Knee Pads',
-    'Masks'
-]
+SLOTS = {
+    'Body Armor': Gear,
+    'Knee Pads': Gear,
+    'Masks': Gear
+}
 
 
-def add_item(name, slot):
-    '''Add a new item with the given name to the backpack'''
-    new_item = Item(name, slot)
-    INVENTORY.append(new_item)
+def add_gear(name, slot, firearms):
+    '''Add a new gear item to the backpack'''
+    new_gear = Gear(name, slot, firearms)
+    INVENTORY.append(new_gear)
     return True
 
 
