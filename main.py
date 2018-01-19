14411eb4348a9c467168083c371d9cd302891d8b
Tester <testwulf@gmail.com>
2018-01-19 15:29:34 -0500

    Extended add item command. Added command to list items.

--- main.py
+++ main.py
@@ -9,14 +9,29 @@ from ui.prompt import Prompt
 def add_item():
     '''Add an item to the inventory'''
     name = input('What is the item named? ')
-    if backpack.add_item(name):
+    print('Which slot does it use?')
+    for index, possible_slot in enumerate(backpack.SLOTS):
+        print(' %s] %s' % (index, possible_slot))
+    slot = int(input('(Enter the slot number): '))
+    slot = backpack.SLOTS[slot]
+    if backpack.add_item(name, slot):
         print('%s added to the backpack' % name)
+
+
+def list_items():
+    '''List all items in the inventory'''
+    item_list = backpack.get_items()
+    for item in item_list:
+        print('Name: %s (%s)' % (item.name, item.slot))
 
 
 # Command Registry
 COMMANDS = {
     'add': {
         'item': add_item
+    },
+    'list': {
+        'items': list_items
     }
 }
 
