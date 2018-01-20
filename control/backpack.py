fb81fb3e7bd4be3a082560945082740b2ddda7bd
Testwulf <testwulf@gmail.com>
2018-01-19 21:19:39 -0500

    Integrated cache controller. Added a function to add loadouts.

--- control/backpack.py
+++ control/backpack.py
@@ -1,11 +1,20 @@
 '''The backpack and inventory controller'''
 
 
-from model.backpack import Item, Gear
+from model.backpack import Gear, Loadout
+from . import cache
 
 
 # Master Inventory List
+INVENTORY_CACHE = 'inventory.pkl'
 INVENTORY = []
+if cache.exists(INVENTORY_CACHE):
+    INVENTORY = cache.get(INVENTORY_CACHE)
+# List of saved loadouts
+LOADOUT_CACHE = 'loadouts.pkl'
+LOADOUTS = []
+if cache.exists(LOADOUT_CACHE):
+    LOADOUTS = cache.get(LOADOUT_CACHE)
 # Possible Gear Slots
 SLOTS = {
     'Body Armor': Gear,
@@ -18,6 +27,15 @@ def add_gear(name, slot, firearms):
     '''Add a new gear item to the backpack'''
     new_gear = Gear(name, slot, firearms)
     INVENTORY.append(new_gear)
+    cache.put(INVENTORY, INVENTORY_CACHE)
+    return True
+
+
+def add_loadout(name):
+    '''Add a loadout to the backpack'''
+    new_loadout = Loadout(name)
+    LOADOUTS.append(new_loadout)
+    cache.put(LOADOUTS, LOADOUT_CACHE)
     return True
 
 
